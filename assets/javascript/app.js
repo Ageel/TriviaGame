a04352cced2ea5b86ab66504ee120ee292855296
Ageel <ashatry@gmail.com>
2017-04-09 15:14:32 -0500

    trying out the answers counters

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,31 +3,32 @@ var incorrect = 0;
 var unanswered = 0;
 var number = 30;
 var intervalId;
+
 $(".game").hide();
 $(".result").hide();
 
 
 $(".startNowButton").on("click", showTheGame);
-
 $(".submitAnswersButton").on("click", showResult);
 
+//showTheGame function shows the game section and hides the startNowButton.
 function showTheGame () {
 	$(".game").show();
 	$(".startNowButton").hide();
 	run();
 }
-
+//showResult function: hides the game section and shows the result section.
 function showResult () {
 	$(".game").hide();
 	$(".result").show();
-	alert("Another of Erik's instruction");
+	// alert("Another of Erik's instruction");
+	// question1();
+	// question2();
+	// question3();
 }
 
-
-
-    // var intervalId;
-
     $(".submitAnswersButton").on("click", stop);
+    $(".submitAnswersButton").on("click", checkAnswers);
 
     function run() {
       intervalId = setInterval(decrement, 1000);
@@ -61,18 +62,52 @@ function showResult () {
       showResult();
     }
 
-    // if (name == "q1" && value == "HTML"){
-    // 	correct++;
-    // }
-    // else if (name !=="q1" && value !=="HTML"){
-    // 	incorrect++;
-    // }
-    // else {
-    // 	unanswered++;
-    // }
+function checkAnswers(){
+	function question1 (){
+	    if (name == "q1" && value == "HTML"){
+    	correct++;
+	    }
+	    else if (name =="q1" && value !=="HTML"){
+	    	incorrect++;
+	    }
+	    else {
+	    	unanswered++;
+	    }
+	    // console.log(question1);
+	}
+
+	function question2 (){
+	    if (name == "q2" && value == "CSS"){
+    	correct++;
+	    }
+	    else if (name =="q2" && value !=="CSS"){
+	    	incorrect++;
+	    }
+	    else {
+	    	unanswered++;
+	    }
+	    // console.log(question2);
+	}
+
+	function question3 (){
+	    if (name == "q3" && value == "CSS"){
+    	correct++;
+	    }
+	    else if (name =="q3" && value !=="CSS"){
+	    	incorrect++;
+	    }
+	    else {
+	    	unanswered++;
+	    }
+	    // console.log(question3);
+	}
+
+}
 
 
-
+console.log(correct);
+console.log(incorrect);
+console.log(unanswered);
     
 
 
